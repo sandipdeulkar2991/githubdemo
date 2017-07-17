913ee4ee4b882e241dadb396e15a03c53983ba35
sandip <sandipdeulkar2991@gmail.com>
2017-07-18 00:05:24 +0530

    initial check

---
+++ githubdemo/src/test/Hello.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Hello {
+public void m1(){
+	System.out.println("m1 of hello class");
+}
+}
---
+++ githubdemo/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+public static void main(String[] args) {
+	Hello h=new Hello();
+	h.m1();
+	m1();
+	
+	
+	
+}
+public static void m1(){
+	System.out.println("m1 of test class");
+}
+}
